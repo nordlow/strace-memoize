92b91abd4064b5874b97dec2febf78d19e061986
Per Nordlöw <per.nordlow@gmail.com>
2017-01-27 15:59:01 +0100

    Add test function

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*! \file main.cpp
+ * \brief
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+int main(int argc, const char * argv[], const char * envp[])
+{
+    return 0;
+}
