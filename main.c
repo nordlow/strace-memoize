8af1afe29d69504b6e0c1971008b25506efd6389
Per Nordlow <per.nordlow@autoliv.com>
2017-01-30 14:20:50 +0100

    Fix

---
+++ main.c
@@ -0,0 +1,12 @@
+/*! \file main.c
+ * \brief
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, const char * argv[], const char * envp[])
+{
+    return 0;
+}
