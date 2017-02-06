8bcb2ee7ec71e693e61d169ae62e6ce70395c6c4
Per Nordlöw <per.nordlow@gmail.com>
2017-02-06 22:38:55 +0100

    Fix

---
+++ test/g++/main.cpp
@@ -0,0 +1,15 @@
+/*! \file main.cpp
+ * \brief
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+int main(int argc, const char * argv[], const char * envp[])
+{
+    int x = 123;
+    return 0;
+}
